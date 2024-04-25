ce04fb4a158438efb35fcbbf4e113d1e4ce942ad
Alexis Perrier <alexis.perrier@gmail.com>
2024-04-25 15:47:57 +0200

    predict endpoint

--- api/main.py
+++ api/main.py
@@ -6,20 +6,20 @@
 from dags.features import FeatureProcessor, FeatureSets
 from fastapi import FastAPI
 
-raw_data = {
-    # 'n_dpe': '2391T1055502K',
-    # 'etiquette_dpe': 'B',
-    # 'etiquette_ges': 'A',
-    'version_dpe': '2.2',
-    'periode_construction': '1983-1988',
-    'secteur_activite': 'W : Administrations, banques, bureaux',
-    'type_energie_principale_chauffage': '',
-    'type_energie_n_1': 'Électricité',
-    'type_usage_energie_n_1': "périmètre de l'usage inconnu",
-    'surface_utile': '100.0',
-    'conso_kwhep_m2_an': '81.1',
-    'conso_e_finale_energie_n_1': '2474.3'
-}
+# raw_data = {
+#     # 'n_dpe': '2391T1055502K',
+#     # 'etiquette_dpe': 'B',
+#     # 'etiquette_ges': 'A',
+#     'version_dpe': '2.2',
+#     'periode_construction': '1983-1988',
+#     'secteur_activite': 'W : Administrations, banques, bureaux',
+#     'type_energie_principale_chauffage': '',
+#     'type_energie_n_1': 'Électricité',
+#     'type_usage_energie_n_1': "périmètre de l'usage inconnu",
+#     'surface_utile': '100.0',
+#     'conso_kwhep_m2_an': '81.1',
+#     'conso_e_finale_energie_n_1': '2474.3'
+# }
 
 
 
@@ -33,3 +33,16 @@ async def root():
 @app.get("/bonjour/{name}")
 async def bonjour(name: str):
     return {"message": f"Bonjour {name}"}
+
+
+# test with
+# curl -X POST
+@app.post("/predict/")
+async def predict(data: dict):
+    return dict
+
+# curl --header "Content-Type: application/json" \
+#      --data "$(cat ~/sample.json)" \
+#      http://20.51.187.25:8000/predict/
+
+#     #  --trace-ascii trace-json-data-request.log \
--- shell/startup.sh
+++ shell/startup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 cd /home/alexisperrier/airflow
+docker-compose down
 docker-compose up -d
 
 cd /home/alexisperrier/ademe_mlops/
+python3 -m pip install --user virtualenv
+python3 -m virtualenv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+git pull origin master
+
 uvicorn api.main:app --host 0.0.0.0 &
 
