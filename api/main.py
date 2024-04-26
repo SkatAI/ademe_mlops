339f77ad32fb36eca99e30c4324354e4b012e1e4
Alexis Perrier <alexis.perrier@gmail.com>
2024-04-26 16:47:56 +0200

    predict

--- api/main.py
+++ api/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-
+import mlflow
+import pandas as pd
+from dags import FeatureProcessor
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
 
 @app.get("/bonjour/{name}")
 async def bonjour(name:str):
@@ -15,3 +16,20 @@ async def bonjour(name:str):
 async def plus(a: int, b: int):
     print("addition")
     return {"message": a+b}
+
+mlflow.set_tracking_uri("http://20.51.187.25:5001")
+mlflow.set_experiment("dpe_tertiaire")
+
+@app.post("/predict/")  # Change to POST method
+async def predict(data: Data):  # Use the Pydantic model for data validation
+    raw_data = pd.DataFrame([data.data])
+    fp = FeatureProcessor(raw_data, "etiquette_dpe")
+    fp.process()
+
+    champ = mlflow.sklearn.load_model(f"models:/dpe_champion/Staging")
+    print(champ)
+    yhat = champ.best_estimator_.predict(fp.data)
+    return {"message": {
+                "prediction": str(yhat[0])
+            }
+        }
